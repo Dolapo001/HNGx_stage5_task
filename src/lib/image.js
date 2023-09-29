4664c94dab2572c0959659ac208875fe7084b663
Mozibix <opuzmoses@gmail.com>
2023-09-29 08:52:52 +0100

    chores: updated imports  image.js

---
+++ src/lib/image.js
@@ -0,0 +1,5 @@
+/* CONDUCT ALL IMAGE IMPORTS HERE */
+
+export { default as placeholder_img } from "../assets/placeholder_img.svg";
+export { default as logo_icon } from "../assets/logo_icon.svg";
+//
