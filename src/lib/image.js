72aca9948b56a7917482b8c5fc08c6927f3de178
Mozibix <opuzmoses@gmail.com>
2023-09-29 10:04:00 +0100

    chores: updated imports  image.js

--- src/lib/image.js
+++ src/lib/image.js
@@ -2,4 +2,9 @@
 
 export { default as placeholder_img } from "../assets/placeholder_img.svg";
 export { default as logo_icon } from "../assets/logo_icon.svg";
-//
+export { default as edit_icon } from "../assets/edit_icon.svg";
+export { default as video_img } from "../assets/video_img.svg";
+export { default as copy_icon } from "../assets/copy_icon.svg";
+export { default as fb_icon } from "../assets/fb_icon.svg";
+export { default as whatsapp_icon } from "../assets/whatsapp_icon.svg";
+export { default as telegram_icon } from "../assets/telegram_icon.svg";
