be03eb08cefccf4b9fd0e4003b0c00eea88a3e76
Mozibix <opuzmoses@gmail.com>
2023-09-30 12:45:24 +0100

    Merge branch 'dark9t_branch' into iseoluwa-branch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,8 +21,6 @@ import Footer from "../components/Footer";
 import Header from "./Header";
 
 export default function Home() {
-  
-
   return (
     <Fragment>
       <Helmet>
@@ -85,7 +83,10 @@ export default function Home() {
 
           <div className="md:px-[80px] px-16 pt-20 pb-[70px] border-b border-b-[30px]">
             <div className="text-center">
-              <p id="features" className="text-[#141414] sm:text-4xl text-2xl pb-8 font-bold ">
+              <p
+                id="features"
+                className="text-[#141414] sm:text-4xl text-2xl pb-8 font-bold "
+              >
                 Features
               </p>
               <p className="text-[#535353] pb-20 ">
@@ -145,7 +146,10 @@ export default function Home() {
           </div>
 
           <div className="md:px-[80px] px-16 py-[70px]">
-            <h3 id="work" className="text-[#120b48] text-2xl sm:text-3xl text-center pb-[50px] font-bold">
+            <h3
+              id="work"
+              className="text-[#120b48] text-2xl sm:text-3xl text-center pb-[50px] font-bold"
+            >
               How It Works
             </h3>
             <div className="md:grid md:grid-cols-3 md:gap-10">
